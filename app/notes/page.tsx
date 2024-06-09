e2507d4b44b5789dcb84f6aad200055f936497ca
mousdieng <moizadieng@gmail.com>
2024-06-09 18:35:41 +0000

    fixing some

---
+++ app/notes/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { createClient } from '../../utils/supabase/server';
+
+export default async function Notes() {
+    const supabase = createClient();
+    const { data: notes } = await supabase.from("notes").select();
+
+    return <pre>{JSON.stringify(notes, null, 2)}</pre>
+}
