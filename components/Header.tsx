943597c30bfb9d7393d4d99fda0349a847386339
mousdieng <moizadieng@gmail.com>
2024-07-03 18:32:48 +0000

    pagination, search

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,14 +7,14 @@ import { DatePickerWithRange } from './ui/date-picker-with-range';
 import Image from 'next/image';
 
 import Landing from './ui/landing';
-// import { QuickSearch } from './ui/search';
+import { QuickSearch } from './ui/search';
 
 export default function Header() {
   return (
     <div className="flex justify-center w-full desktop:min-h-screen">
       <div className='flex flex-col items-center w-full max-w-7xl p-3 pb-14 text-sm space-y-10 desktop:justify-around relative desktop:pb-0 border-2 border-b-custom-dark-60'>
         <Landing />
-        {/* <QuickSearch /> */}
+        <QuickSearch />
       </div>
     </div>
   );
--- components/ui/search.tsx
+++ components/ui/search.tsx
@@ -1,57 +1,57 @@
-// "use client"
-// import React, { Fragment, useCallback, useEffect, useState } from 'react'
-// import {
-//     Card,
-//     CardContent,
-//     CardDescription,
-//     CardFooter,
-//     CardHeader,
-//     CardTitle,
-// } from "@/components/ui/card"
-// import { Input } from './input'
-// import { SearchIcon, SlidersHorizontalIcon } from 'lucide-react'
-// import { clsx } from "clsx";
-// import { Popover, Transition } from "@headlessui/react"
-// import { Button } from './button'
-// import Filter from './filter'
-// import { useRouter, useSearchParams } from 'next/navigation'
-// import throttle from '@/lib/throttle'
+"use client"
+import React, { Fragment, useCallback, useEffect, useState } from 'react'
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Input } from './input'
+import { SearchIcon, SlidersHorizontalIcon } from 'lucide-react'
+import { clsx } from "clsx";
+import { Popover, Transition } from "@headlessui/react"
+import { Button } from './button'
+import Filter from './filter'
+import { useRouter, useSearchParams } from 'next/navigation'
+import throttle from '@/lib/throttle'
 
-// export function QuickSearch() {
-//     const router = useRouter();
-//     const searchParams = useSearchParams();
+export function QuickSearch() {
+    const router = useRouter();
+    const searchParams = useSearchParams();
 
-//     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-//         e.preventDefault();
-//         const formData = new FormData(e.currentTarget);
-//         const origin = formData.get('origin') as string;
-//         const destination = formData.get('destination') as string;
-//         const currentParams = new URLSearchParams(Array.from(searchParams.entries()));
-//         if (origin) {
-//             currentParams.set('origin', origin);
-//         }
-//         if (destination) {
-//             currentParams.set('destination', destination);
-//         } 
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const origin = formData.get('origin') as string;
+        const destination = formData.get('destination') as string;
+        const currentParams = new URLSearchParams(Array.from(searchParams.entries()));
+        if (origin) {
+            currentParams.set('origin', origin);
+        }
+        if (destination) {
+            currentParams.set('destination', destination);
+        } 
 
-//         router.replace(`/annonce?${currentParams}`)
-//     }
+        router.replace(`/annonce?${currentParams}`)
+    }
 
-//     return (
-//         <Card className='bg-custom-light-98 rounded-md space-y-5 p-4 w-full max-w-lg desktop:max-w-full desktop:w-full desktop:flex-row desktop:space-y-0 desktop:space-x-5 desktop:p-8'>
-//             <CardHeader>
-//                 <CardDescription>Trouver des command en cour</CardDescription>
-//             </CardHeader>
-//             <CardContent className='p-0'>
-//                 <form method='get' onSubmit={handleSubmit} className='flex flex-col gap-4 desktop:flex-row'>
-//                     <Input type='text' placeholder='Oringine...' name="origin"/>
-//                     <Input type='text' placeholder='Destination...' name="destination" />
-//                     <Button type='submit' className='bg-custom-sky-50 hover:bg-custom-sky-60'>Chercher</Button>
-//                 </form>
-//             </CardContent>
-//         </Card>
-//     )
-// }
+    return (
+        <Card className='bg-custom-light-98 rounded-md space-y-5 p-4 w-full max-w-lg desktop:max-w-full desktop:w-full desktop:flex-row desktop:space-y-0 desktop:space-x-5 desktop:p-8'>
+            <CardHeader>
+                <CardDescription>Trouver des command en cour</CardDescription>
+            </CardHeader>
+            <CardContent className='p-0'>
+                <form method='get' onSubmit={handleSubmit} className='flex flex-col gap-4 desktop:flex-row'>
+                    <Input type='text' placeholder='Oringine...' name="origin"/>
+                    <Input type='text' placeholder='Destination...' name="destination" />
+                    <Button type='submit' className='bg-custom-sky-50 hover:bg-custom-sky-60'>Chercher</Button>
+                </form>
+            </CardContent>
+        </Card>
+    )
+}
 
 // export default function Search() {
 //     const [input, setInput] = useState<string>("")
