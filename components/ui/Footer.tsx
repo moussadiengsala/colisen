b2df686a6414b5531d2f87373a3685470b404afd
mousdieng <moizadieng@gmail.com>
2024-07-09 00:08:00 +0000

    completing some features

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Socialmedia from './socialmedia'
+import { Button } from './button'
+import Link from 'next/link'
+import { FacebookIcon, PhoneIcon } from 'lucide-react'
 
 export default function Footer() {
     return (
@@ -7,7 +10,22 @@ export default function Footer() {
             <div className='w-full max-w-7xl p-3 text-sm'>
                 <div className='flex flex-col-reverse gap-4 tablet:gap-0 tablet:flex-row justify-between items-center w-full'>
                     <p>2024 <strong>Colisen</strong>. Tous droits réservés.</p>
-                    <Socialmedia isLabelNeeded={false} />
+                    <div className='flex flex-col gap-2'>
+                        <Button asChild className='bg-transparent hover:bg-transparent text-custom-dark-40 hover:text-custom-dark-60 font-semibold p-0'>
+                            <Link href="#" className='flex gap-2 items-center'>
+                                <FacebookIcon />
+                                <span>Visite our page facebook</span>
+                            </Link>
+                        </Button>
+                        <div className='flex gap-2 items-center text-custom-dark-40 hover:text-custom-dark-60 font-semibold'>
+                            <PhoneIcon />
+                            <span>+221 78 000 00 00</span>
+                        </div>
+                        <div className='flex gap-2 items-center text-custom-dark-40 hover:text-custom-dark-60 font-semibold'>
+                            <PhoneIcon />
+                            <span>+221 70 000 00 00</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </footer>
